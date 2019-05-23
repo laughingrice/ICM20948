990713666ccfade4fd04c012a6bb6cc3d2b3b3e6
Micha <michaf@mit.edu>
2019-05-23 17:39:42 -0400

    Updated TODO comments

--- Code/ICM20948/__main__.py
+++ Code/ICM20948/__main__.py
@@ -1,11 +1,8 @@
-# TODO: Read raw acc, gyro and compass data
-# TODO: setup DPM
-# TODO: read DPM Data
-# TODO: setup FIFO
-# TODO: work into Versatile framework
+# Raw magnetometer data (is it accessible over SPI?)
+# DPM Data
+# Incorporate in Versatile Framework
 
 from .ICM20948 import ICM20948
 
 imu = ICM20948()
-
 print(imu.measure())
