6660c245b2085b0e0cdc346f09bdda4c442dd020
Micha <micha.feigin@gmail.com>
2019-07-01 22:53:28 -0400

    Retry initialization 10 times in case of failure

--- Code/ICM20948/__main__.py
+++ Code/ICM20948/__main__.py
@@ -7,6 +7,16 @@ from .ICM20948 import ICM20948
 
 imu = ICM20948()
 
+for i in range(10):
+    try:
+        imu._setup()
+    except Exception as e:
+        print('got exception: {} - trying again'.format(e.args[0]))
+
+    if imu.AK09916_initialized:
+        break
+
+
 # Print out reading every second
 while True:
     print('  '.join(['{:> 7.3}'.format(float(x)) for x in imu.measure()]))
